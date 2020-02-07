99f367aa7e3b39eea0883f5de8a38035157b5db2
LoganTann <logon313@hotmail.fr>
2020-02-07 22:52:30 +0100

    remake the database editor code

--- scan/utils/databaseEditor.php
+++ scan/utils/databaseEditor.php
@@ -14,17 +14,18 @@ $manga = "kagerou-days"; //TODO : add security
  	</head>
  	<body class="container">
 		<h1>Kagescan Database Editor</h1>
-		Manga : <?php echo $manga ?>
-
-		<button type="button" class="btn right" name="save" disabled>Save</button>
-
+		<p>Manga : <?php echo $manga ?></p>
+		<p>
+			<button type="button" name="goToMenu" onclick="app.applyDefaultDisplay()" class="btn">Go back (Volume Selection)</button>
+			<button type="button" class="btn right" name="save" disabled>Save</button>
+		</p>
 		<main id="app" style="margin-top: 20px;" class="row hide-on-small-only">
 			<div class="col m6">
 				<div class="row" id="list">
 				</div>
 			</div>
 			<div class="col m6" id="editContainer">
-				<div id="default" class="active">
+				<div id="defaultEditorMsg" class="active">
 					<p>Click on the volume to select it and edit his informations.<br>
 					Click again to show and edit the chapters</p>
 				</div>
@@ -46,122 +47,119 @@ $manga = "kagerou-days"; //TODO : add security
 		<p class="hide-on-med-and-up">Your screen is too small !! Rotate your phone, enable desktop mode or use a computer/larger screen to use the database editor.</p>
  	</body>
 
-	<script>
-		//vars
-		var globals = {
-			volume: -1,
-			chapter: -1,
-			getRoot: function(){
-				if (globals.volume == -1) {
-					return 0;
-				} else {
-					return (globals.chapter != -1) ? 2 : 1;
-				}
-			},
-			goBack: function() {
-				const r = globals.getRoot();
-				if (r > 1) { // is chapter edit
-					globals.chapter = -1;
-				}
-				if (r > 0) { // is volume edit
-					globals.volume = -1;
-				}
-			}
-		};
+<script>
+	//vars
+	var globals = {
+		volume: -1,
+		chapter: -1
+	};
 
-		// helpers
-		function hideActive(query, target){
-			const bef = document.querySelector(`${query}.active`);
-			if (bef) {
-				bef.classList.remove("active");
-			}
-			target.classList.add("active");
+	// helpers
+	function exchangeActiveClass(query, target){
+		const bef = document.querySelector(`${query}.active`);
+		if (bef) {
+			bef.classList.remove("active");
 		}
-
-		// main
-		var app = {
-			start: function(){
-				app.elem = document.getElementById("app");
-				app.title = document.getElementById("title");
-				app.list = document.getElementById("list");
-				app.chapterEdit = document.getElementById("chapterEdit");
-				app.volumeEdit = document.getElementById("volumeEdit");
-				app.default = document.getElementById("default");
-				app.renderList();
-			},
-
-			renderList: function(){
-				if (globals.getRoot() == 1) { // chapters edit
-					app.list.innerHTML = "";
-
-					const backBtn = document.createElement("a");
-					backBtn.innerText = "Go back";
-					backBtn.addEventListener("click", function(){
-						console.log("back");
-					});
-					list.append(backBtn);
-
-					//app.list;
-					for (const i in db.volumes[globals.volume].chapters) {
-						const chapter = db.volumes[globals.volume].chapters[i];
-						const a = document.createElement("a");
-						a.innerText = chapter.name;
-						a.href = `#${i}`;
-						a.addEventListener("click", function(){
-							globals.chapter = i;
-							app.updateEditor();
-						});
-
-						a.classList.add("col", "s12");
-						if (i==0) a.classList.add("active");
-
-						list.append(a);
-						console.log(chapter);
-					}
-					console.log("hello");
-				} else {
-					for (const i in db.volumes) {
-						const a = document.createElement("a");
-						a.innerText = db.volumes[i].name;
-						a.href = `#${i}`;
-						a.addEventListener("click", function(){
-							if (this.classList.contains("active")){
-								globals.volume = i;
-							} else {
-								globals.volume = -1;
-								hideActive("#list a", this );
-							}
-							app.updateEditor(this);
-						});
-						a.classList.add("col", "s12");
-						app.list.append(a);
-					}
-				}
-			},
-
-			updateEditor: function(before){
-				switch (globals.getRoot()) {
-					case 0:
-						hideActive("#editContainer>div", app.volumeEdit);
-						console.log("top");
-						break;
-					case 1:
-						console.log("volume");
-						hideActive("#editContainer>div", app.chapterEdit);
-						app.renderList();
-						globals.chapter = 0;
-						// don't break.
-					case 2:
-						console.log("chapter");
-						break;
-				}
+		target.classList.add("active");
+	}
+	function cleanButIgnore( parent, toIgnore ){
+		while (parent.childNodes.length > 1) {
+			if (parent.lastChild != toIgnore) {
+				parent.removeChild(parent.lastChild);
+			} else if (parent.firstChild != toIgnore) {
+				parent.removeChild(parent.firstChild);
 			}
 		}
-		document.addEventListener('DOMContentLoaded', function() {
-			app.start();
-		});
-		var db = <?php include("../$manga/manga.json") ?>;
-	</script>
+	}
+
+	// main
+	var app = {
+		start: function() {
+			app.elem = document.getElementById("app");
+			app.title = document.getElementById("title");
+			app.list = document.getElementById("list");
+			app.chapterEdit = document.getElementById("chapterEdit");
+			app.volumeEdit = document.getElementById("volumeEdit");
+			app.defaultEditorMsg = document.getElementById("defaultEditorMsg");
+
+			app.applyDefaultDisplay();
+		},
+
+		// general
+		applyDefaultDisplay: function() {
+			app.generateVolumeChoice();
+			app.displayDefaultEditorMsg();
+
+			document.querySelector("button[name='goToMenu']").style.display = "none";
+		},
+
+		// Events
+		volumeBtnOnClick: function() {
+			if (! this.classList.contains("active")){
+				this.classList.add("active");
+				app.generateChapterChoice(this);
+				app.generateVolumeEditor(this);
+			}
+		},
+		chapterBtnOnClick: function() {
+			if (! this.classList.contains("active")){
+				exchangeActiveClass("#list a", this);
+				app.generateChapterEditor(this);
+			}
+		},
+
+		// Choice section
+		generateVolumeChoice: function() {
+			app.list.innerHTML = "";
+
+			for (const i in db.volumes) {
+				app.addChoiceBtn( (btn) => {
+					btn.innerText = db.volumes[i].name;
+					btn.href = `#${i}`;
+					btn.onclick = app.volumeBtnOnClick;
+					btn.dataset.volumeIndex = i;
+				} );
+			}
+		},
+		generateChapterChoice: function( parentVolumeBtn ) {
+			cleanButIgnore( app.list, parentVolumeBtn );
+
+			const chapterList = db.volumes[ parentVolumeBtn.dataset.volumeIndex ].chapters;
+			for (const i in chapterList) {
+				app.addChoiceBtn( (btn) => {
+					btn.innerText = chapterList[i].name;
+					btn.href = `#${i}`;
+					btn.onclick = app.chapterBtnOnClick;
+					btn.dataset.chapterIndex = i;
+				} );
+			}
+		},
+		addChoiceBtn: function(callback) {
+			const choice = document.createElement("a");
+			choice.classList.add("col", "s12");
+			if (typeof(callback) == "function") {
+				callback(choice);
+			}
+			app.list.append(choice);
+		},
+
+		// Editor section
+		displayDefaultEditorMsg: function() {
+			exchangeActiveClass("#editContainer>div", app.defaultEditorMsg);
+		},
+		generateVolumeEditor: function( parentVolumeBtn ) {
+			document.querySelector("button[name='goToMenu']").style.display = "initial";
+			exchangeActiveClass("#editContainer>div", app.volumeEdit);
+		},
+		generateChapterEditor: function( parentChapterBtn) {
+			exchangeActiveClass("#editContainer>div", app.chapterEdit);
+		}
+	}
+	document.addEventListener('DOMContentLoaded', function() {
+		app.start();
+	});
+	var db = <?php include("../$manga/manga.json") ?>;
+</script>
 
 	<style>
 		@font-face {

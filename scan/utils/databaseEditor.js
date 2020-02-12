2834c80a303f4927d094b42afec6ef6de06523df
LoganTann <logon313@hotmail.fr>
2020-02-12 16:28:18 +0100

    make the remove chapter/volume feature + starting new diff function - This is now possible to delete a chapter or an entire volume - Create a function that converts a database into text - Remove the diff functions : I will use a php function instead (added but not implemented yet), that support delete edits. It will be uglier and it's a pain to delete what was done before, but we need that.
    Abort todos : 
    - Move chapters : too difficult to do and not very useful when we can 
    edit directly the files
    - Autosave : not useful
    
    New todos :
    -make the invert of the db to text converter

--- scan/utils/databaseEditor.js
+++ scan/utils/databaseEditor.js
@@ -1,12 +1,12 @@
 // helpers
-function exchangeActiveClass(query, target){
+function exchangeActiveClass(query, target) {
 	const bef = document.querySelector(`${query}.active`);
 	if (bef) {
 		bef.classList.remove("active");
 	}
 	target.classList.add("active");
 }
-function cleanButIgnore( parent, toIgnore ){
+function cleanButIgnore( parent, toIgnore ) {
 	while (parent.childNodes.length > 1) {
 		if (parent.lastChild != toIgnore) {
 			parent.removeChild(parent.lastChild);
@@ -14,6 +14,27 @@ function cleanButIgnore( parent, toIgnore ){
 			parent.removeChild(parent.firstChild);
 		}
 	}
+}
+
+function getActivePositionFromHisParent(parent) {
+	for (let i = 0; i < parent.childNodes.length; i++) {
+		if (parent.childNodes[i].classList.contains("active")) {
+			return i;
+		}
+	}
+}
+
+function prettyPrintDb(obj) {
+	let retval = "";
+	for (const vol of obj.volumes) {
+		retval += `(${vol.id}) ${vol.name} : \n\t- Preview Image : ${vol.coverArt}\n\t- Summary : >>${vol.summary.replace(/\n/g, ">>")}\n`;
+		for (const chap  of vol.chapters) {
+			retval += `\t(${chap.id}) ${chap.name} : \n\t\t- Preview Image : ${chap.previewImg}\n\t\t- Summary : >>${chap.summary.replace(/\n/g, ">>")}\n`;
+		}
+		retval += `\n\n`;
+	}
+	// testing only
+	app.databaseRaw.value = retval;
 }
 
 // main
@@ -31,6 +52,7 @@ var app = {
 		app.databaseDiffContainer = document.getElementById("databaseDiffContainer");
 		app.databaseRaw = document.getElementById("databaseRaw");
 		app.saveBtn = document.getElementById("saveBtn");
+		app.listControlBtns = document.getElementById("listControlBtns");
 
 		app.initElements();
 		app.applyDefaultDisplay();
@@ -46,18 +68,19 @@ var app = {
 	},
 
 	// general
-	applyDefaultDisplay: function(isInit) {
+	applyDefaultDisplay: function(doSaveLocalValues = true) {
 		if (app.elem.classList.contains("hide")) {
 			app.elem.classList.remove("hide");
 			app.sendToServerPopup.classList.add("hide");
 			app.saveBtn.classList.remove("hide");
 		}
-
-		app.saveLocalValuesFromEditor();
+		if (doSaveLocalValues) {
+			app.saveLocalValuesFromEditor();
+		}
 		app.generateVolumeChoice();
 		app.displayDefaultEditorMsg();
 
-		document.querySelector("button[name='goToMenu']").style.display = "none";
+		app.listControlBtns.style.display = "none";
 	},
 
 	// Events
@@ -79,11 +102,47 @@ var app = {
 			}
 		}
 	},
+	removeActive: function() {
+		const activeIndex = getActivePositionFromHisParent(app.list);
+		if (typeof(activeIndex) !== "number") {
+			throw "Cannot find the index of the active element in the button list";
+		}
+		let targetElem = app.list.childNodes[activeIndex];
+		let isVolume = typeof(targetElem.dataset.volumeIndex) == "string";
+		if (isVolume || app.list.childNodes.length < 4) {
+			if (! confirm("You are about to remove an entire volume. Are you sure ??")) {
+				return "cancelled";
+			}
+		}
+
+		if (targetElem.dataset.chapterIndex) {
+			const volumeIndex = app.list.firstChild.dataset.volumeIndex;
+			const chapterIndex = targetElem.dataset.chapterIndex;
+			if (! db.volumes[volumeIndex]) {
+				throw `Error : db.volumes[${volumeIndex}] is undefined`;
+			}
+			const chapterList = db.volumes[volumeIndex].chapters;
+			chapterList.splice(chapterIndex, 1);
+			let toUnderline = document.querySelector(`#list a[href="#${chapterList.length - 1}"][data-chapter-index]`);
+
+			if (toUnderline) {
+				app.generateChapterEditor(toUnderline);
+				app.generateChapterChoice(toUnderline);
+			} else {
+				isVolume = true;
+				targetElem = app.list.firstChild;
+			}
+		}
+		if (isVolume) {
+			const volumeIndex = targetElem.dataset.volumeIndex;
+			db.volumes.splice(volumeIndex, 1);
+			app.applyDefaultDisplay(false);
+		}
+	},
 
 	// Choice section
 	generateVolumeChoice: function() {
 		app.list.innerHTML = "";
-
 		for (var i = 0; i <= db.volumes.length; i++) {
 			app.addChoiceBtn( (btn) => {
 				btn.innerText = (i == db.volumes.length)? "Create Volume": db.volumes[i].name;
@@ -104,13 +163,17 @@ var app = {
 		const chapterList = db.volumes[ volumeIndex ].chapters;
 		for (var i = 0; i <= chapterList.length; i++) {
 			app.addChoiceBtn( (btn) => {
-				btn.innerText = (i == chapterList.length)? "Create Chapter": chapterList[i].name;
-				btn.onclick = app.chapterBtnOnClick;
+				if (i == chapterList.length) {
+					btn.innerText = "Create Chapter"
+				} else {
+					btn.dataset.chapterIndex = i;
+					btn.innerText = chapterList[i].name;
+				}
 				btn.href = `#${i}`;
-				btn.dataset.chapterIndex = i;
+				btn.onclick = app.chapterBtnOnClick;
 			} );
 		}
-		if (! document.querySelector("#list a.active")) {
+		if ( parentVolumeBtn.innerText == "Create Chapter" || !document.querySelector("#list a.active")) {
 			document.querySelector(`#list a[href="#${chapterList.length - 1}"]`).classList.add("active");
 		}
 	},
@@ -128,7 +191,7 @@ var app = {
 		exchangeActiveClass("#editContainer>div", app.defaultEditorMsg);
 	},
 	generateVolumeEditor: function( parentVolumeBtn ) {
-		document.querySelector("button[name='goToMenu']").style.display = "block";
+		app.listControlBtns.style.display = "block";
 		exchangeActiveClass("#editContainer>div", app.volumeEdit);
 
 		let volumeInfo = db.volumes[ parentVolumeBtn.dataset.volumeIndex ];
@@ -223,106 +286,8 @@ var app = {
 		document.execCommand("copy");
 	},
 
-	// diff functions (might hurt your eyes)
 	displayDatabaseDiff: function(){
-		// reset
-		app.databaseDiffContainer.innerHTML = "";
-		const changes = app.createDiffBetween2Objects( app.databaseBeforeEdit.volumes, db.volumes);
-		const   infos = document.createElement("div");
-		changes.id = "databaseDiff";
-		  infos.id = "showSelectedDiff";
-		changes.classList.add("col", "m6");
-		  infos.classList.add("col", "m6", "grey", "lighten-3");
-		app.databaseDiffContainer.append( changes, infos );
-
-		// stuff
-		const allVolumeElems = document.querySelectorAll("#databaseDiff>li > ul");
-		for (const volElem of allVolumeElems) {
-
-			const volumeTitle = document.createElement("b");
-			const volumeInfos = db.volumes[volElem.dataset.index];
-			volumeTitle.innerText = volumeInfos.id;
-			volElem.before(volumeTitle);
-
-			const allChapElems = volElem.querySelectorAll(`[data-index="chapters"] ul`);
-			if (allChapElems) {
-				for (chapElem of allChapElems) {
-					const chapTitle = document.createElement("b");
-					chapTitle.innerText = volumeInfos.chapters[ chapElem.dataset.index ].name;
-					chapElem.before(chapTitle);
-				}
-			}
-		}
-	},
-	showLongChange: function(newValue, oldValue) {
-		const target = document.getElementById("showSelectedDiff");
-		target.innerHTML = "";
-		if (oldValue) {
-			target.innerHTML = `
-				<h3>Original Value</h3>
-				<em class="red-text">${oldValue.replace(/\n/g, "<br>")}</em>
-			`;
-		}
-		target.innerHTML += `
-			<h3>New Value</h3>
-			<em class="green-text">${(newValue) ? newValue.replace(/\n/g, "<br>") : "[deleted]"}</em>
-		`;
-	},
-	createDiffBetween2Objects: function(old, now) {
-		const retval = document.createElement("ul");
-		retval.classList.add("browser-default");
-		// TODO: Support delete type
-
-		for (const i in now) {
-			const li = document.createElement("li");
-			if (old[i]) {
-				li.classList.add("orange-text");
-				if (typeof(old[i]) == "object") {
-					const eval = app.createDiffBetween2Objects(old[i], now[i]);
-					if (eval.childNodes.length > 0){
-						eval.dataset.index = i;
-						li.append (eval);
-					}
-				} else if (old[i] != now[i]) {
-					li.innerText = i;
-					if (now[i].length + old[i].length > 50) {
-						li.innerHTML += " <i class='material-icons'>info</i>";
-						li.addEventListener("mouseover", function(){
-							exchangeActiveClass("#databaseDiff ", li);
-							app.showLongChange(now[i], old[i]);
-						});
-					} else {
-						li.innerHTML += ` :
-							<span class="red-text">  ${old[i]}</span> ->
-							<span class="green-text">${now[i]}</span>`;
-					}
-				}
-			} else if (now[i]) { // Old[i] empty and now[i] not empty -> value created
-				li.classList.add("green-text");
-				if (typeof(now[i]) == "object") {
-					const eval = app.createDiffBetween2Objects({}, now[i]);
-					if (eval.childNodes.length > 0){
-						eval.dataset.index = i;
-						li.append (eval);
-					}
-				} else {
-					li.innerText = i;
-					if (now[i].length > 50) {
-						li.innerHTML += " <i class='material-icons'>info</i>";
-						li.addEventListener("mouseover", function(){
-							exchangeActiveClass("#databaseDiff ", li);
-							app.showLongChange(now[i]);
-						});
-					} else {
-						li.innerText += ` : ${now[i]}`;
-					}
-				}
-			}
-			if (li.childNodes.length > 0) {
-				retval.append(li);
-			}
-		}
-		return retval;
+		// TBA
 	}
 }
 document.addEventListener('DOMContentLoaded', function() {
